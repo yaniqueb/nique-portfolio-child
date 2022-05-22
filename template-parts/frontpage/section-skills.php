89fdf3bba86bb1b14b4cf734a295b0267a0c2897
Yanique Bullock <yanique.b@gmail.com>
2022-05-22 19:37:14 -0400

    added wysiwyg to skills

--- template-parts/frontpage/section-skills.php
+++ template-parts/frontpage/section-skills.php
@@ -1,26 +1,14 @@
+<?php 
+$skills = get_field('skills');
+?>
+
 <section id="skills" class="section section--sec">
-						<div class="section__container site-wrapper">
-							<header class="section__header">
-								<h2 class="section__title">Skills</h2>
-							</header><div class="section__box section__box--row">
-								<ul class="section__list">
-									<li><h3 class="section__list-title">Front-end</h3></li>
-									<li class="section__list-item">HTML</li>
-									<li class="section__list-item">CSS</li>
-									<li class="section__list-item">JavaScript</li>
-									<li class="section__list-item">Bootstrap</li>
-									<li class="section__list-item">sass</li>
-								</ul>
-								<ul class="section__list">
-									<li><h3 class="section__list-title">Back-end</h3></li>
-									<li class="section__list-item">PHP</li>
-									</ul>
-									<ul class="section__list">
-										<li><h3 class="section__list-title">Other</h3></li>
-										<li class="section__list-item">Git</li>
-										<li class="section__list-item">WordPress (CRM)</li>
-										<li class="section__list-item">Google Analytics</li>
-									</ul>
-								</div>
-							</div>
-					</section>
+	<div class="section__container site-wrapper">
+		<header class="section__header">
+			<h2 class="section__title">Skills</h2>
+		</header>
+		<div class="section__box section__box--row">
+			<?php echo esc_html( the_field('skills')); ?>
+		</div>
+	</div>
+</section>
